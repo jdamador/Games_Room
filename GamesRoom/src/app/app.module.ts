a8ef62c1121ef142369fdd52a2e0eda761b85f72
Rebeca Bolaños Arce <rebebolaos.98@gmail.com>
2019-02-24 18:55:14 -0600

    Inicio de Sesión
    Inicio de Sesión y Vista principal

--- GamesRoom/src/app/app.module.ts
+++ GamesRoom/src/app/app.module.ts
@@ -1,12 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Reactive Form
+import { ReactiveFormsModule } from "@angular/forms";
+
+// App routing modules
+import { AppRoutingModule } from './shared/routing/app-routing.module';
+
+// App components
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+// Firebase services + enviorment module
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+// Auth service
+import { AuthService } from "./shared/services/auth.service";
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyA5P5GimK9Sy7zhWUCFgoWHe-0LNLtnsfA",
+      authDomain: "proyect1design.firebaseapp.com",
+      databaseURL: "https://proyect1design.firebaseio.com",
+      projectId: "proyect1design",
+      storageBucket: "proyect1design.appspot.com",
+      messagingSenderId: "965759997972"
+    }),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    ReactiveFormsModule
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule { }
--- GamesRoom/src/environments/environment.ts
+++ GamesRoom/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
 };
 
 /*
