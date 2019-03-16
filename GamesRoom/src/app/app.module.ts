4f0115e3edc8abe9e1f26ed80a01ac46441e470e
jdamador <amadorsalasjd@gmail.com>
2019-03-15 18:52:02 -0600

    feat: add memory component

--- GamesRoom/src/app/app.module.ts
+++ GamesRoom/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule} from '@angular/material/card';
-import { 
+import { MatCardModule } from '@angular/material/card';
+import {
   MatButtonModule,
   MatToolbarModule,
   MatSidenavModule,
   MatIconModule,
   MatListModule,
-  MatTableModule, MatPaginatorModule, MatSortModule
- } from '@angular/material'
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+} from '@angular/material';
 
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Reactive Form
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 // App routing modules
 import { AppRoutingModule } from './shared/routing/app-routing.module';
@@ -22,25 +24,27 @@ import { AppRoutingModule } from './shared/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { SignUpComponent} from './components/sign-up/sign-up.component';
-import { VerifyEmailComponent} from './components/verify-email/verify-email.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 // Firebase services + enviorment module
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
 // Auth service
-import { AuthService } from "./shared/services/auth.service";
-import { UserService } from "./shared/user-service/user.service"; 
+import { AuthService } from './shared/services/auth.service';
+import { UserService } from './shared/user-service/user.service';
 import { from } from 'rxjs';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { SaveListComponent } from './components/save-list/save-list.component';
+import { MemoryBoardComponent } from './components/memory-board/memory-board.component';
 
-
+// Services
+import { CardService } from './shared/services/card.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,6 +55,7 @@ import { SaveListComponent } from './components/save-list/save-list.component';
     ProfileComponent,
     VerifyEmailComponent,
     SaveListComponent,
+    MemoryBoardComponent
   ],
   imports: [
     BrowserModule,
@@ -72,8 +77,7 @@ import { SaveListComponent } from './components/save-list/save-list.component';
     MatPaginatorModule,
     MatSortModule
   ],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, CardService],
   bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
