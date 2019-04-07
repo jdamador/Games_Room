3fff650d02dd3ac5b4d3becf9f4b9ed4d847e00f
jdamador <amadorsalasjd@gmail.com>
2019-04-07 09:50:06 -0600

    feat: memory board

--- GamesRoom/src/app/components/memory-board/memory-board.component.ts
+++ GamesRoom/src/app/components/memory-board/memory-board.component.ts
@@ -7,9 +7,23 @@ import { MemoryService } from 'src/app/shared/services/memory/memory.service';
   styleUrls: ['./memory-board.component.css']
 })
 export class MemoryBoardComponent implements OnInit {
-  cards = [];
-  constructor(private memory: MemoryService) {}
+  // Cards will be show in the game board.
+  cards: [];
+  session: any;
+
+  // Default image, it is shows when the card is not flipped.
+  images_inact = '/assets/Memory/poker.png';
+
   ngOnInit() {
-    
+    // When the board component is called, it get a new board from the API.
+    this.memoryService.getAllBoard().subscribe(response => {
+      this.cards = response['grid'];
+      this.session = response;
+    });
   }
+
+  constructor(private memoryService: MemoryService) {}
+
+  // When a card is clicked an event is throw.
+  card_selected(i) {}
 }
--- GamesRoom/src/app/shared/services/memory/memory.service.ts
+++ GamesRoom/src/app/shared/services/memory/memory.service.ts
@@ -1,36 +1,59 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable, observable } from 'rxjs';
-import { messaging } from 'firebase';
+import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoryService {
+
   private url = 'http://localhost:3000';
-  private socket;
+  private userInfo = this.authService.userData;
 
-  constructor() {
-    this.socket = io(this.url);
+  constructor(private authService: AuthService, private http: HttpClient) {}
+
+  public connectToServer(id: string) {
+    const socket = io.connect(this.url, { foceNew: true });
+    this.joinGame(socket, id);
+    return socket;
   }
-  getCurrentBoard() {
-    let socket = this.socket;
-    let observable = new Observable(function(observer) {
-      socket.on('currentboard', function(data) {
-        observer.next(data);
-      });
-    });
-
-    return observable;
+  private joinGame(socket: any, id: string) {
+    socket.emit('join-game', { gameID: id, username: this.userInfo });
   }
-  getInitialBoard() {
-    let socket = this.socket;
-    let observable = new Observable(function(observer) {
-      socket.on('initialboard', function(data) {
-        observer.next(data);
-      });
-    });
 
-    return observable;
+  public playerMove(socket: any, gameUpdate: any) {
+    return socket.emit('player-move', gameUpdate);
+  }
+
+  public gameUpdated(socket: any, updateFunction) {
+    socket.on('game-updated', updateFunction);
+  }
+
+  public gameOver(socket: any, gameOverFunction) {
+    socket.on('game-over', gameOverFunction);
+  }
+
+  public resetGame(socket: any, gameStatus: any) {
+    socket.emit('reset-game', {
+      username: this.userInfo.email,
+      gameStatus: gameStatus
+    });
+  }
+
+  public disconnect(socket: any, disconnectFunction) {
+    socket.on('opponent left', disconnectFunction);
+  }
+
+  public waitingForOpponent(socket: any, waitingFunction) {
+    socket.on('waiting for opponent', waitingFunction);
+  }
+
+  public disconnectSession(socket: any) {
+    socket.disconnect();
+  }
+  getAllBoard(): Observable<any> {
+    return this.http.get<any>('http://localhost:3000/getBoard');
   }
 }
