ede29731a86a289ddae4b5cdc3fa0b544d418933
Usuario <Usuario@LABICSC1-73574.itcr.ac.cr>
2019-04-07 21:37:24 -0600

    Mover fichas Damas

--- GamesRoom/src/app/components/checkers-board/checkers-board.component.ts
+++ GamesRoom/src/app/components/checkers-board/checkers-board.component.ts
@@ -15,6 +15,7 @@ export class CheckersBoardComponent implements OnInit {
     yActual : string;
     ganador : string;
     estado = "false";
+    num = 1;
 
   constructor(private checkersService: CheckersService) { }
 
@@ -33,28 +34,39 @@ export class CheckersBoardComponent implements OnInit {
   }
 
   //Envia información hacia el servicio para verificar los posible movimientos de una ficha
-  envioInfoVerficarPosiblesMovimiento(){
-    this.checkersService.envioInfoPosibleMovimiento({"jugador": this.uidJugador1, "idSala":this.idSala, "x": this.xActual, "y":this.yActual})
+  async envioInfoVerficarPosiblesMovimiento(){
+    this.checkersService.envioInfoPosibleMovimiento({"jugador": this.uidJugador1, "idSala":this.idSala, "x": this.xActual, "y":this.yActual});
+    await this.getPosiblesMovimientos();
   }
 
   //Obtiene del servicio los posibles movimientos de una ficha
-  getPosiblesMovimientos(){
-    this.checkersService.getPosiblesMovimientos()
+  async getPosiblesMovimientos(){
+    this.checkersService.getPosiblesMovimientos().elementAt(this.num)
     .subscribe((data)=>{
+      this.num ++;
       this.idSala = data.idSala;
       this.tablero = data.tablero;
       this.ganador = data.ganador;
+      console.log(data.ganador);
+      console.log(this.ganador);
       console.log("Tablero Posibles Movimientos: " + data.tablero);
-    })
+      if(this.ganador == "noturno"){
+        alert("¡No es tu turno!");
+      }
+      else{
+        this.estado = "true";
+        console.log("Estado = " + this.estado);
+      }
+    });
   }
   
   //Envia información hacia el servicio para actualizar la tabla con un nuevo movimiento
-  envioInfoActualizarTableroNuevoMovimiento(){
+  async envioInfoActualizarTableroNuevoMovimiento(){
     this.checkersService.envioInfoActualizarTableroNuevoMovimiento({"idSala": this.idSala, "x": this.xActual, "y":this.yActual})
   }
 
   //Obtiene del servicio los posibles movimientos de una ficha
-  getTableroNuevoMovimiento(){
+  async getTableroNuevoMovimiento(){
     this.checkersService.getTableroNuevoMovimiento()
     .subscribe((data)=>{
       this.idSala = data.idSala;
@@ -62,29 +74,26 @@ export class CheckersBoardComponent implements OnInit {
       this.ganador = data.ganador;
       console.log("Tablero Nuevo Movimientos: " + data.tablero);
     })
-    
+
   }
 
-  function(row , col){
+  async function(row , col){
     this.xActual = row;
     this.yActual = col;
-
     if(this.estado == "false"){
       if(this.tablero[this.xActual][this.yActual] != 'V'){
-        this.envioInfoVerficarPosiblesMovimiento();
-        this.getPosiblesMovimientos();
-        this.estado = "true";
+        await this.envioInfoVerficarPosiblesMovimiento();
+        console.log(this.ganador);
       }
     }
 
     else if(this.estado == "true"){
-      this.envioInfoActualizarTableroNuevoMovimiento();
-      this.getTableroNuevoMovimiento();
-      this.estado = "false";
-      console.log("Entó al else");
+      if(this.tablero[this.xActual][this.yActual] != 'V'){
+        this.envioInfoActualizarTableroNuevoMovimiento();
+        this.getTableroNuevoMovimiento();
+        this.estado = "false";
+        console.log("Estado = " + this.estado);
+      }
     }
   }
-
-
-
 }
